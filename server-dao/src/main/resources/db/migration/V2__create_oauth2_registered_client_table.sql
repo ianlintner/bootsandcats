b3546a7711c4b88df33a951f56ed0a510693bebf
Ian Lintner <lintner.ian@gmail.com>
2025-12-03 22:48:43 -0600

    fix: Correct timestamp data types in oauth2_registered_client table and enable Flyway migrations for tests

--- server-dao/src/main/resources/db/migration/V2__create_oauth2_registered_client_table.sql
+++ server-dao/src/main/resources/db/migration/V2__create_oauth2_registered_client_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS oauth2_registered_client (
     id VARCHAR(100) PRIMARY KEY,
     client_id VARCHAR(100) NOT NULL UNIQUE,
-    client_id_issued_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    client_id_issued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     client_secret VARCHAR(200),
-    client_secret_expires_at TIMESTAMPTZ,
+    client_secret_expires_at TIMESTAMP WITH TIME ZONE,
     client_name VARCHAR(200) NOT NULL,
     client_authentication_methods TEXT NOT NULL,
     authorization_grant_types TEXT NOT NULL,
