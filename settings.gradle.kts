4ea1f4f093408c0a4cd3f99b1a3c44069fa3f959
Ian Lintner <lintner.ian@gmail.com>
2025-12-03 18:10:51 -0600

    chore: Include Gatling project in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,12 @@ include(
     "server-dao",
     "server-logic",
     "server-ui",
-    "canary-app"
+    "canary-app",
+    "gatling"
 )
 
 project(":server-dao").projectDir = file("server-dao")
 project(":server-logic").projectDir = file("server-logic")
 project(":server-ui").projectDir = file("server-ui")
 project(":canary-app").projectDir = file("canary-app")
+project(":gatling").projectDir = file("gatling")
