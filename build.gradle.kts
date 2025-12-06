c23a473008e2fa88ebf7d0eb0654ed979567eb3f
Ian Lintner <lintner.ian@gmail.com>
2025-12-06 10:05:47 -0600

    feat: Upgrade Spring Boot version to 4.0.0 in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "3.4.1" apply false
+    id("org.springframework.boot") version "4.0.0" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     kotlin("jvm") version "2.1.0" apply false
     id("com.diffplug.spotless") version "6.25.0"
