5e2719aed6fc26ba48def951b9ea104c52e36324
Ian Lintner <lintner.ian@gmail.com>
2025-12-06 11:57:15 -0600

    feat: Implement Profile UI application with JWT decoder factory setup

---
+++ profile-ui/src/main/java/com/bootsandcats/profile/CanaryApplication.java
@@ -0,0 +1,24 @@
+package com.bootsandcats.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
+
+@SpringBootApplication
+public class ProfileUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileUiApplication.class, args);
+    }
+
+    @Bean
+    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
+        OidcIdTokenDecoderFactory idTokenDecoderFactory = new OidcIdTokenDecoderFactory();
+        idTokenDecoderFactory.setJwsAlgorithmResolver(
+                clientRegistration -> SignatureAlgorithm.ES256);
+        return idTokenDecoderFactory;
+    }
+}
