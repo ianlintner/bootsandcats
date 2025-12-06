74f18ba67281ea72634eff3e11d0c7ef3d185e8d
Ian Lintner <lintner.ian@gmail.com>
2025-12-05 18:56:29 -0600

    feat: Add JwtDecoderFactory bean for OIDC token decoding

--- canary-app/src/main/java/com/bootsandcats/canary/CanaryApplication.java
+++ canary-app/src/main/java/com/bootsandcats/canary/CanaryApplication.java
@@ -2,10 +2,22 @@ package com.bootsandcats.canary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
 
 @SpringBootApplication
 public class CanaryApplication {
     public static void main(String[] args) {
         SpringApplication.run(CanaryApplication.class, args);
+    }
+
+    @Bean
+    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
+        OidcIdTokenDecoderFactory idTokenDecoderFactory = new OidcIdTokenDecoderFactory();
+        idTokenDecoderFactory.setJwsAlgorithmResolver(clientRegistration -> SignatureAlgorithm.ES256);
+        return idTokenDecoderFactory;
     }
 }
