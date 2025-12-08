7aee9c56087663eafa9df92da1b7449ca7a9d217
Ian Lintner <lintner.ian@gmail.com>
2025-12-08 17:11:49 -0600

    refactor: Remove unused imports in ProfileControllerTest

--- profile-ui/src/test/java/com/bootsandcats/profileui/controller/ProfileControllerTest.java
+++ profile-ui/src/test/java/com/bootsandcats/profileui/controller/ProfileControllerTest.java
@@ -15,13 +15,10 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
-import io.micronaut.security.authentication.Authentication;
-import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import java.time.Instant;
-import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
