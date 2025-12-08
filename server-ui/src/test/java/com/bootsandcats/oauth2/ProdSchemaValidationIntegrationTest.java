0947b4327b525d234f9faca984ff2e82725d3f93
Ian Lintner <lintner.ian@gmail.com>
2025-12-08 15:58:50 -0600

    feat: Add JUnit Tag annotation for testcontainers in ProdSchemaValidationIntegrationTest

--- server-ui/src/test/java/com/bootsandcats/oauth2/ProdSchemaValidationIntegrationTest.java
+++ server-ui/src/test/java/com/bootsandcats/oauth2/ProdSchemaValidationIntegrationTest.java
@@ -3,6 +3,7 @@ package com.bootsandcats.oauth2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 @ActiveProfiles("prod")
 @Testcontainers
+@Tag("testcontainers")
 @TestPropertySource(
         properties = {
             "spring.session.store-type=none",
