747ffa93249787728eb62e8ab047591e4efbfbd9
Ian Lintner <lintner.ian@gmail.com>
2025-12-10 08:35:10 -0600

    feat: add NoOpPriorToLoginPersistence to support Micronaut security without OAuth2

---
+++ profile-service/server/src/main/java/com/bootsandcats/profileui/security/NoOpPriorToLoginPersistence.java
@@ -0,0 +1,25 @@
+package com.bootsandcats.profileui.security;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.errors.PriorToLoginPersistence;
+import jakarta.inject.Singleton;
+import java.net.URI;
+import java.util.Optional;
+
+/**
+ * No-op PriorToLoginPersistence to satisfy Micronaut security beans when OAuth2 login is not used.
+ */
+@Singleton
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class NoOpPriorToLoginPersistence implements PriorToLoginPersistence {
+    public void persist(Object request, Object response, Object context) { }
+
+    public Optional retrieve(Object request) { return Optional.empty(); }
+
+    public void remove(Object request) { }
+
+    public Optional<URI> getOriginalUri(Object request, Object response) { return Optional.empty(); }
+
+    public void onUnauthorized(Object request, Object response) { }
+
+}
